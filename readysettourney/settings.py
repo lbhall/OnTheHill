0674701fbcdf750025454a8b106424908a37024d
Beau Hall <beauhalliv@gmail.com>
2026-06-30 09:16:46 -0500

    updating settings for production

--- readysettourney/settings.py
+++ readysettourney/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,11 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%&$p36e_!%l!@y$+6vvuo4i7@d^#b0x8vvpfwf9_2+$d8kz-lh'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-%&$p36e_!%l!@y$+6vvuo4i7@d^#b0x8vvpfwf9_2+$d8kz-lh')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'false').lower() == 'true'
 
 ALLOWED_HOSTS = ['readysettourney.com', 'www.readysettourney.com']
 
